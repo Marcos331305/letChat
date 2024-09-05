a8af926b1e5db6045240fc1716d3f10ed8bcf23e
Marcos331305 <139604169+Marcos331305@users.noreply.github.com>
2024-09-06 00:37:20 +0530

    added userName on conversation page and added functionality to chat-Btn

---
+++ scripts/conversation.js
@@ -0,0 +1,5 @@
+// fetching elements
+const userName = document.querySelector('.js-userName');
+
+// getting personUserName from localStorage and display on page
+userName.textContent = localStorage.getItem('personUsername')
--- scripts/people.js
+++ scripts/people.js
@@ -2,16 +2,16 @@
 import { supabase } from "../scripts/supaBase.js";
 
 // fetching elements
-const people = document.querySelector('.js-people');
+const people = document.querySelector(".js-people");
 
 // generating HTML for the people
-function peopleHTML(users){
-    let html = ``;
-    users.forEach((user)=>{
-        const userName = user.username;
-        html += `
+function peopleHTML(users) {
+  let html = ``;
+  users.forEach((user) => {
+    const userName = user.username;
+    html += `
                     <!-- person -->
-                    <div class="flex justify-between border-b-[1px] px-4 py-1">
+                    <div class="flex justify-between border-b-[1px] px-4 py-1 js-person">
                     <!-- Avatar & userName -->
                     <div class="flex items-center gap-6">
                         <div>
@@ -21,29 +21,38 @@ function peopleHTML(users){
                             alt="Avatar-Logo"
                         />
                         </div>
-                        <div class="text-white text-xl">
+                        <div class="text-white text-xl js-person-username">
                             ${userName}
                         </div>
                     </div>
                     <!-- message-icon -->
                     <div>
-                        <img class="h-[48px]" src="./assets/chat-icon.png" alt="chat-icon">
+                        <img class="h-[48px] cursor-pointer js-chat-btn" src="./assets/chat-icon.png" alt="chat-icon">
                     </div>
                     </div>
-                `;        
-    });
-    people.innerHTML = html;
+                `;
+  });
+  people.innerHTML = html;
 }
 
 // fetching users from the dB
 async function fetchUsersFromDb() {
   const { data, error } = await supabase.from("users").select("username");
-  if(data){
-    console.log(data)
+  if (data) {
     const users = data;
     peopleHTML(users);
-  } else{
-    people.textContent = 'Server not Respond !'
+  } else {
+    people.textContent = "Server not Respond !";
   }
 }
 fetchUsersFromDb();
+
+// adding functionality to chat-Btn
+document.addEventListener('click',(event) => {
+    if (event.target.classList.contains('js-chat-btn')) {
+        const person = event.target.closest('.js-person');
+        const personUsername = person.querySelector('.js-person-username').innerText;
+        localStorage.setItem('personUsername',personUsername);
+        window.location.href = '/conversation.html';
+    }
+});
